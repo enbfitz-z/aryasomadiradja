0f33e032d1d65346efe01f79b36162fb8398cb25
enbfitz-z <radjaarya4@gmail.com>
2026-06-23 21:34:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,37 +6,21 @@ document.addEventListener("mousemove", function(e){
     document.body.style.backgroundPosition =
     x/50 + "px " + y/50 + "px";
 
-    function planetInfo(planet){
+function planetInfo(planet) {
 
-const data = {
+    const data = {
+        merkurius: "Merkurius adalah planet terdekat dengan Matahari.",
+        venus: "Venus adalah planet terpanas.",
+        bumi: "Bumi adalah tempat tinggal manusia.",
+        mars: "Mars dikenal sebagai Planet Merah.",
+        jupiter: "Jupiter adalah planet terbesar.",
+        saturnus: "Saturnus memiliki cincin yang indah.",
+        uranus: "Uranus berputar miring.",
+        neptunus: "Neptunus adalah planet terjauh."
+    };
 
-merkurius:
-"Merkurius adalah planet terdekat dengan Matahari. Suhu siang hari bisa mencapai 430°C.",
-
-venus:
-"Venus adalah planet terpanas di Tata Surya dengan suhu sekitar 475°C.",
-
-bumi:
-"Bumi adalah planet ketiga dari Matahari dan satu-satunya planet yang diketahui memiliki kehidupan.",
-
-mars:
-"Mars dikenal sebagai Planet Merah karena permukaannya mengandung banyak besi oksida.",
-
-jupiter:
-"Jupiter adalah planet terbesar di Tata Surya dan memiliki Bintik Merah Besar.",
-
-saturnus:
-"Saturnus terkenal karena cincin indah yang mengelilinginya.",
-
-uranus:
-"Uranus berputar miring hampir 98 derajat sehingga tampak seperti berguling saat mengorbit Matahari.",
-
-neptunus:
-"Neptunus adalah planet terjauh dari Matahari dan memiliki angin tercepat di Tata Surya."
-
-};
-
-alert(data[planet]);
+    alert(data[planet]);
+}
 
         window.addEventListener("load", () =>{
     document.getElementById("loader").style.display="none";
